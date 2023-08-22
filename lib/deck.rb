a5073b486fbd99157ddc142acedbbc15558be21c
Scott DeVoss <scottdevoss@yahoo.com>
2023-08-22 16:11:53 -0600

    add deck class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,14 @@
 class Deck
-  def initialize[card_1, card_2, card_3]
+  attr_reader :cards, :count, :category_deck
+  def initialize(cards)
+    @cards = cards
+    @count = cards.length
+    @category_deck = []
+  end 
+
+  def cards_in_category(category_filter)
+    @category_deck = @cards.select do |card| 
+      card.category == category_filter
+    end 
   end 
 end 
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,4 +1,38 @@
-require './lib/card'
 require './lib/deck'
-require 'rspec'
+require './lib/card'
+require 'pry'
+
+RSpec.describe Deck do
+  card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card_1, card_2, card_3]
+  it 'exists' do
+    deck = Deck.new(cards)
+
+    expect(deck).to be_instance_of(Deck)
+  end 
+
+  it 'has cards' do
+    deck = Deck.new(cards)
+    expect(deck.cards).to eq(cards)
+  end 
+
+  it 'has a count' do
+    deck = Deck.new(cards)
+    expect(deck.count).to eq(3)
+  end 
+
+  it 'contains cards with a specific category' do
+    deck = Deck.new(cards) 
+    expect(deck.cards_in_category(:STEM)).to eq([card_2, card_3])
+  end 
+
+  it 'has no cards with a specific category' do
+    deck = Deck.new(cards)
+    expect(deck.cards_in_category('Pop Culture')).to eq([])
+  end 
+  
+end
+
 
